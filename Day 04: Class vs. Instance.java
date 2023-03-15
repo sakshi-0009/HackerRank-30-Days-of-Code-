796d22e5bc9ba94fc9693978014878c0178cc09f
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-03-15 07:17:31 +0530

    Update Day 04: Class vs. Instance.java

--- Day 04: Class vs. Instance.java
+++ Day 04: Class vs. Instance.java
@@ -17,7 +17,6 @@ public class Solution {
         private int age;
 
         public Person(int initialAge) {
-            // Add some more code to run some checks on initialAge
             if (initialAge > 0) {
                 age = initialAge;
             } else {
@@ -27,14 +26,12 @@ public class Solution {
         }
 
         public void amIOld() {
-            // Write code determining if this person's age is old and print the correct statement:
             if (age < 13) System.out.println("You are young.");
             else if (age < 18) System.out.println("You are a teenager.");
             else System.out.println("You are old.");
         }
 
         public void yearPasses() {
-            // Increment this person's age.
             age++;
         }
     }
